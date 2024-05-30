8abaf1ffe4cdb77e16c04a45684fef8f616d4c84
SeekerDev <albertbaisa@gmail.com>
2024-05-30 12:01:50 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Suspense, useEffect, useState } from "react";
 import Header from "./layout/header/Header";
-
 import Footer from "./layout/footer/Footer";
 import LoginForm from "./layout/header/loginform/LoginForm";
 import { lazy } from "react";
-
-import { ToastContainer } from "react-toastify";
+import { motion } from "framer-motion";
 import "react-toastify/dist/ReactToastify.css";
 
 const Content = lazy(() => import("./layout/content/Content"));
@@ -15,6 +13,7 @@ function App() {
   const [isOpenLogin, setOpenLogin] = useState(false);
   const [isLoggedIn, setLoggedIn] = useState(false);
   const [userData, setUserData] = useState();
+  const text = "WELCOME TO iMONITOR";
 
   useEffect(() => {
     const currentUser = JSON.parse(window.localStorage.getItem("CurrentUser"));
@@ -34,14 +33,19 @@ function App() {
         setUserData={setUserData}
         userData={userData}
       />
-      <header className="w-screen bg-slate-400 h-[60px]">
+      <motion.header
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 3 }}
+        className="w-screen bg-slate-400 h-[60px]"
+      >
         <Header
           setOpenLogin={setOpenLogin}
           isOpenLogin={isOpenLogin}
           isLoggedIn={isLoggedIn}
           userData={userData}
         />
-      </header>
+      </motion.header>
 
       {isLoggedIn ? (
         <Suspense fallback="Loading...">
@@ -55,11 +59,37 @@ function App() {
           </div>
         </Suspense>
       ) : (
-        <></>
+        <div className="h-full w-full flex flex-col mt-[30vh] items-center text-[6vh] font-bold text-white gap-2  tracking-widest">
+          <h1 className="bold text-[40px] mx-8 flex font-bold  tracking-[10px]">
+            <motion.div className="text">
+              {text.split("").map((letter, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.2, delay: index * 0.1 }}
+                >
+                  {letter}
+                </motion.span>
+              ))}
+            </motion.div>
+          </h1>
+          <motion.div
+            initial={{ width: 0 }}
+            animate={{ width: "70vh" }}
+            transition={{ duration: 1, delay: 2 }}
+            className="w-[70vh] h-[1px] bg-white"
+          />
+        </div>
       )}
-      <footer className="bg-slate-600 fixed w-full bottom-0">
+      <motion.footer
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 3 }}
+        className="bg-slate-600 fixed w-full bottom-0"
+      >
         <Footer />
-      </footer>
+      </motion.footer>
     </main>
   );
 }
